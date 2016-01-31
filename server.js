dac7c6296cc3c1cea6bca5bd6b2ad1e5fb986ebb
amjedonline <amjedonline@gmail.com>
2016-01-31 21:25:31 +0100

    Adding update/put method for beer model.

--- server.js
+++ server.js
@@ -25,27 +25,9 @@ router.get('/', function(req, res) {
   res.json({ message: 'You are running dangerously low on beer!' });
 });
 
-// Add Post request handling
+
+// Add /beers route request handling
 var beersRoute = router.route('/beers');
-
-//get all
-beersRoute.get(function(req, res){
-  Beer.find(function(err, beers){
-    if(err)
-      res.send(err);
-    res.json(beers);
-  })
-});
-
-//get one
-var beerRoute = router.route('/beers/:beer_id');
-beerRoute.get(function(req, res) {
-  Beer.findById(req.params.beer_id, function(err, beer){
-    if (err)
-      res.send(err)
-    res.json(beer);
-  });
-});
 
 //Create endpoint /api/beers for POSTS
 beersRoute.post(function(req, res){
@@ -63,9 +45,45 @@ beersRoute.post(function(req, res){
 
     res.json({message: 'Beer added to the locker!', data:beer });
   })
-  
+
 });
 
+
+//get all
+beersRoute.get(function(req, res){
+  Beer.find(function(err, beers){
+    if(err)
+      res.send(err);
+    res.json(beers);
+  })
+});
+
+
+//get one
+var beerRoute = router.route('/beers/:beer_id');
+beerRoute.get(function(req, res) {
+  Beer.findById(req.params.beer_id, function(err, beer){
+    if (err)
+      res.send(err)
+    res.json(beer);
+  });
+});
+
+beerRoute.put(function(req, res) {
+  Beer.findById(req.params.beer_id, function(err, beer){
+    if (err)
+      res.send(err)
+
+    beer.quantity = req.body.quantity;
+
+    // Let us now save the beer
+    beer.save(function(err){
+      if (err)
+        res.send(err)
+      res.json(beer);
+    });
+  });
+});
 
 // Register all our routes with /api
 app.use('/api', router);
