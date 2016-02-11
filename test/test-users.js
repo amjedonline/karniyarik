d67d9504959f4ee96bb5bc10576f83113c01bc1d
amjedonline <amjedonline@gmail.com>
2016-02-11 23:48:28 +0100

    Adding test case for user post.

--- test/test-users.js
+++ test/test-users.js
@@ -5,22 +5,23 @@ var server = require('../server.js');
 var should = chai.should();
 
 var authHeader = '';
+var registrationHeader = '';
 var authUser = 'amjed';
 var authPass = 'amjed';
+var registrationId = 'id123id';
 
 chai.use(chaiHttp);
 
 describe('Users', function() {
 
-
-
   before(function(done) {
     chai.request(server)
     .post('/api/authentication/jwt_token')
-    .send({username: authUser, password: authPass, registration_id: 'id123id'})
+    .send({username: authUser, password: authPass, registration_id: registrationId})
     .end(function(err, res) {
         res.should.have.status(200);
         authHeader = {'Authorization': 'Bearer '+ res.body.token};
+        registrationHeader = {'RegistrationId': registrationId};
         done();
     });
   });
@@ -29,6 +30,7 @@ describe('Users', function() {
       chai.request(server)
       .get('/api/users')
       .set(authHeader)
+      .set(registrationHeader)
       .end(function(err, res) {
           res.should.have.status(200);
           res.should.be.json;
@@ -38,7 +40,20 @@ describe('Users', function() {
   });
 
   it('should list a SINGLE user on /user/<id> GET');
-  it('should add a SINGLE user on /users/ POST');
+
+  it('should add a SINGLE user on /users/ POST', function (done){
+    chai.request(server)
+    .post('/api/users')
+    .set(authHeader)
+    .set(registrationHeader)
+    .send({username: 'superman', password: 'superSecret123'})
+    .end(function(err, res) {
+        res.should.have.status(200);
+        res.body.should.be.a('object')
+        done();
+    });
+
+  });
   it('should update a SINGLE user on /user/<id> PUT');
   it('should delete a SINGLE user on /user/<id> DELETE');
 });
